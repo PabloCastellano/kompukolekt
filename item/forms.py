a4b58d54cc3d17cad4898ddf39c208994f250479
Pablo Castellano <pablo@anche.no>
2015-06-23 12:37:03 +0200

    ComputerForm

--- item/forms.py
+++ item/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from item.models import UserProfile
+from item.models import UserProfile, Computer
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -14,3 +14,9 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
+
+
+class ComputerForm(forms.ModelForm):
+    class Meta:
+        model = Computer
+        exclude = ('user', 'created_at', 'updated_at')
